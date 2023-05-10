239a4ee6a1e0d141b9dcf5a24bb132333dd0fe2e
prao7 <prao7@wisc.edu>
2023-05-09 18:27:37 -0600

    Added initial files as well as installed packages

---
+++ productionCostModelingH2DAC/pcmh2dac.jl
@@ -0,0 +1,8 @@
+#=
+Author: Pradyumna Rao, M.S. Mechanical Engineering, University of Colorado-Boulder
+Advisor: Professor Bri-Mathias Hodge, Department of Electrical, Computer and Energy Engineering, University of Colorado-Boulder
+
+The following code builds a production cost model for an optimal scenario using RE-CT's and DAC.
+=#
+using PowerSimulations
+using PowerSystems
