ed4fdda6c90255c96b111bb92b583f9431b258c5
prao7 <prao7@wisc.edu>
2024-07-20 23:00:52 -0600

    removing environment. gridstatus capacity market capabilities limited

--- myenv/lib/python3.12/site-packages/gridstatus/tests/decorators.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-class with_markets:
-    def __init__(self, *markets):
-        self.markets = markets
-
-    def __call__(self, *args, **kwargs):
-        return pytest.mark.parametrize("market", self.markets)(*args, **kwargs)
